f1f0f99a6344a7e2182faf70dfdb0b4af8bb72d8
Taylor Kearns <tkearns@gmail.com>
2011-11-29 06:35:34 -0500

    Values now persist on form. Not sure if it's the best way to do it.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,14 +1,14 @@
 class StatsController < ApplicationController
-  def index  	
-  	def result_count
-  	  params[:result_count].to_i
-  	end 
-  	
-  	def measured_stat
-  		params[:measured_stat].to_s
-  	end
-  	
-  	@measured_stat = self.measured_stat
-  	@results = Stat.where(:year => (params[:start_year])..(params[:end_year])).limit(result_count).reorder("#{ measured_stat } DESC")
-  end
+	def index  	
+		def result_count
+			params[:result_count].to_i
+		end 
+		
+		def measured_stat
+			params[:measured_stat].to_s
+		end
+		
+		@measured_stat = self.measured_stat
+		@results = Stat.where(:year => (params[:start_year])..(params[:end_year])).limit(result_count).reorder("#{ measured_stat } DESC")
+	end
 end
