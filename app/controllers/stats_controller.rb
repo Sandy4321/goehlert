50dc185497dbd44a243c5ff36b624777722178f1
Taylor Kearns <tkearns@gmail.com>
2011-12-15 18:28:10 -0500

    Compiled results now ordered by measured stat and limited to result amount.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -9,9 +9,7 @@ class StatsController < ApplicationController
 		
 	# Raw stats
 	def raw  	
-		def result_count
-			params[:result_count].to_i
-		end 
+		result_count = params[:result_count].to_i
 		
 		@measured_stat = measured_stat
 		@raw_results = Stat.where(:year => params[:start_year]..params[:end_year]).limit(result_count).reorder(measured_stat + ' DESC')
@@ -20,10 +18,11 @@ class StatsController < ApplicationController
 	# Compiled stats
 	def compiled
 		stat = measured_stat()
-		@team = Team.all
+		result_count = params[:result_count].to_i
 		
 		if !stat.nil?
-			@compiled_results = Stat.compiled_stats(stat, params)
+			stat_symbol = stat.to_sym
+			@compiled_results = Stat.compiled_stats(stat, params).sort_by { |team| team[stat_symbol] }.reverse!.first(result_count)
 		end
 	end
 end
