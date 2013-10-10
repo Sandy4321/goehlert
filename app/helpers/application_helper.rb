e1b76247673e252319117037a39549d7d60fc13f
Taylor Kearns <tkearns@gmail.com>
2013-10-10 16:53:41 -0400

    Style updates for stats pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
-module ApplicationHelper  
+module ApplicationHelper
+  def body_classes
+    controller_name
+  end
+
   def cap_first(str)
   	conjunctions = ['or', 'of', 'and']
     words = str.split(' ')
     words.each do |word|
-    	if !conjunctions.include?(word) 
-    		word.to_s.capitalize! 
+    	if !conjunctions.include?(word)
+    		word.to_s.capitalize!
     	end
     end
     words.join(' ')
